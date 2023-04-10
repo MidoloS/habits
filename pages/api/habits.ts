cf959285cc41fc3f3c13e535db5cd2a6d74eeb84
MidoloS <midolo.1912@gmail.com>
2023-04-10 11:55:25 -0300

    habit api

---
+++ pages/api/habits.ts
@@ -0,0 +1,14 @@
+import { generatePrismaClient } from "@/prisma/client";
+import { HabitName, Habit } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = generatePrismaClient();
+
+  const habits = await prisma.habit.findMany();
+
+  return res.status(200).json(habits);
+}
