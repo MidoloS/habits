62310ff0843215064e2042825c1f09cd3d9a94b4
MidoloS <midolo.1912@gmail.com>
2023-05-01 03:04:55 -0300

    toast

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -5,9 +5,44 @@ import Webcam from "react-webcam";
 
 import { getHighestValueKey } from "../libs/helpers";
 
+const TakenToast = ({ name }: { name: string }) => (
+  <div
+    id="toast-undo"
+    className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+    role="alert"
+  >
+    <div className="text-sm font-normal">
+      Habit completed {name.toUpperCase()}
+    </div>
+    <div className="flex items-center ml-auto space-x-2">
+      <button
+        type="button"
+        className="bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target="#toast-undo"
+        aria-label="Close"
+      >
+        <span className="sr-only">Close</span>
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+);
+
 export const Camera = () => {
   const [facing, setFacing] = useState("environment");
-  const [taken, setTaken] = useState(false);
+  const [habitName, setHabitName] = useState<string | null>(null);
 
   const handleFace = () => {
     if (facing === "user") {
@@ -23,7 +58,6 @@ export const Camera = () => {
         audio={false}
         height={720}
         mirrored={facing === "user"}
-        // @ts-ignore
         screenshotFormat="image/png"
         width={1280}
         imageSmoothing={true}
@@ -33,13 +67,13 @@ export const Camera = () => {
         screenshotQuality={1}
         videoConstraints={{
           facingMode: facing,
-          frameRate: { ideal: taken ? 0 : 60, max: taken ? 0 : 60 },
         }}
       >
         {/* @ts-ignore */}
         {({ getScreenshot }) => (
           <button
             onClick={() => {
+              setHabitName(null);
               const base64 = getScreenshot();
 
               if (!base64) {
@@ -58,7 +92,7 @@ export const Camera = () => {
                 })
                 .then((result) => {
                   console.log(result);
-                  alert(getHighestValueKey(result));
+                  setHabitName(getHighestValueKey(result));
                 })
                 .catch((error) => console.log("error", error));
             }}
@@ -68,6 +102,7 @@ export const Camera = () => {
         )}
       </Webcam>
       <button onClick={handleFace}>Switch camera</button>
+      {habitName && <TakenToast name={habitName} />}
     </>
   );
 };
