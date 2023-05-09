a8a41936912cc5ae52e6dd06b6936afa999d6eb3
MidoloS <midolo.1912@gmail.com>
2023-05-09 15:22:48 -0300

    more space

--- app/habit/[name]/scan/page.tsx
+++ app/habit/[name]/scan/page.tsx
@@ -3,7 +3,7 @@ import { Camera } from "@/components/Camera";
 export default async function Page() {
   return (
     <>
-      <div className="max-h-48">
+      <div className="max-h-56">
         <Camera />
       </div>
     </>
