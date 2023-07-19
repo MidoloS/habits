86d1b12245b3160cdeaa2fdac91c8d3f5fe519ea
MidoloS <midolo.1912@gmail.com>
2023-07-19 09:15:07 -0300

    fix out of style buttons

--- components/GoBackButton.tsx
+++ components/GoBackButton.tsx
@@ -3,25 +3,25 @@ import Link from "next/link";
 const GoBackButton = () => {
   return (
     <Link
-      className="bg-slate-200 text-slate-950 h-14 w-14 rounded-lg flex items-center justify-center"
+      className="shadow-sm border border-slate-200 bg-slate-50 text-slate-950 p-4 rounded-lg flex items-center justify-center"
       href="/"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="26.809"
-        height="23.286"
-        viewBox="0 0 26.809 23.286"
+        width="20.722"
+        height="18.182"
+        viewBox="0 0 20.722 18.182"
       >
         <path
           id="Path_92"
           data-name="Path 92"
-          d="M24.5,17l-10,10m0,0L4.5,17m10,10V3"
-          transform="matrix(0.017, 1, -1, 0.017, 28.356, -3.12)"
+          d="M19.5,13.5,12,21m0,0L4.5,13.5M12,21V3"
+          transform="matrix(0.017, 1, -1, 0.017, 22.357, -3.12)"
           fill="none"
           stroke="#020617"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
         />
       </svg>
     </Link>
