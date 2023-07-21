aae9bc4743c8ad98aa2253099a0de3dba9fc0f3b
MidoloS <midolo.1912@gmail.com>
2023-07-21 10:54:41 -0300

    signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,28 @@
 import { GoogleSignInButton } from "@/components/GoogleSignInButton";
+import Image from "next/image";
 
 const SignInPage = () => {
   return (
-    <section className="flex min-h-full overflow-hidden pt-16 sm:py-28">
-      <GoogleSignInButton />
-    </section>
+    <figure className="relative w-full h-screen bg-[#0F1A22] flex flex-col items-center">
+      <Image
+        src="/signin.png"
+        alt="wonderful big tree vectorial design"
+        height={669}
+        width={430}
+      />
+
+      <div className="absolute bottom-0 flex w-full p-8 flex-col gap-10 justify-center items-center">
+        <div>
+          <h1 className="text-center text-slate-50 font-heading font-semibold text-3xl">
+            Begin Journey
+          </h1>
+          <p className="text-slate-400 text-center">
+            Join to other Rankers around the world
+          </p>
+        </div>
+        <GoogleSignInButton />
+      </div>
+    </figure>
   );
 };
 
