75c932b228ed25ad7d8734f001387598530edc68
MidoloS <midolo.1912@gmail.com>
2023-07-21 21:33:03 -0300

    better success modal

--- components/HabitCompleted.tsx
+++ components/HabitCompleted.tsx
@@ -20,16 +20,17 @@ export const HabitCompleted = () => {
   return (
     <div className="w-screen h-screen z-10 bg-black bg-opacity-80 flex items-center justify-center p-8 absolute top-0 ">
       <div className="bg-slate-50 z-20 p-6 rounded-xl gap-4 flex flex-col max-w-md">
-        <h1 className="text-center text-slate-950 text-lg font-semibold">
+        <h1 className="text-center text-slate-950 text-lg font-semibold font-heading">
           Habit Completed
         </h1>
-        <p className="text-center text-slate-500">
-          Congratulations, you have completed the task!
+        <p className="text-center text-slate-500 text-sm">
+          Congratulations, you have completed the task! <br /> Dont forget that
+          its the small accomplishments that make a
+          <span className="text-slate-950 font-semibold">
+            big change over time!
+          </span>
         </p>
-        <p className="text-center text-slate-500">
-          Dont forget that its the small accomplishments that make a big change
-          over time!
-        </p>
+
         <PrimaryButton onClick={handleOnClick}>Great!</PrimaryButton>
       </div>
     </div>
