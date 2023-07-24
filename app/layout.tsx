a69f5a405236c18c16603e9114852ad74ad53c82
MidoloS <midolo.1912@gmail.com>
2023-07-23 21:02:24 -0300

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function BlogLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#020617" />
         <title>habitai.io</title>
       </head>
 
