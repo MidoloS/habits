d80295a11fde12c3e835558d41215edd0b54c9db
MidoloS <midolo.1912@gmail.com>
2023-07-27 03:21:53 -0300

    better <Features/>

--- components/Info/Feature.tsx
+++ components/Info/Feature.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Feature: FC<Props> = ({ children, text }) => (
   <div className="flex flex-col items-center">
-    <div className="bg-slate-100 w-fit p-4 rounded-md">{children}</div>
+    <div className="border w-fit p-4 rounded-xl">{children}</div>
     <div className="mt-2">
       <p className="text-slate-500 text-xs font-sans">{text}</p>
     </div>
