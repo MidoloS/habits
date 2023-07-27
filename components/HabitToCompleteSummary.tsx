370e782219a617fdb6a6d534c2b0373493c19c29
MidoloS <midolo.1912@gmail.com>
2023-07-27 04:52:44 -0300

    summary link

--- components/HabitToCompleteSummary.tsx
+++ components/HabitToCompleteSummary.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { CircularProgressbar } from "./CircularProgressBar";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { SubscriptionWithHabit } from "@/libs/types";
+import Link from "next/link";
 
 export const HabitToCompleteSummary = async () => {
   const session = await getServerSession(authOptions);
@@ -28,17 +29,35 @@ export const HabitToCompleteSummary = async () => {
 
   const title = percentajeCompleted > 50 ? "Good job!" : "Keep going!";
 
+  const nonCompletedHabit = subs.filter(
+    (sub: SubscriptionWithHabit) => !sub.completedAt
+  )[0];
+
+  console.log({ nonCompletedHabit });
+
   return (
-    <div className="bg-slate-950 flex justify-between py-2 px-6 items-center rounded-xl">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-slate-50 font-medium">{title}</h1>
-        <p className="text-slate-500 text-sm">
-          {completedSubs}/{totalSubs} habits completed
-        </p>
-      </div>
-      <div>
-        <CircularProgressbar percentage={percentajeCompleted} strokeWidth={5} />
-      </div>
-    </div>
+    <Link
+      href={`${process.env.NEXTAUTH_URL}/habit/${nonCompletedHabit.habit.name}/complete`}
+      key={nonCompletedHabit.habit.name}
+      passHref
+      legacyBehavior
+    >
+      <a>
+        <div className="bg-slate-950 flex justify-between py-2 px-6 items-center rounded-xl">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-slate-50 font-medium">{title}</h1>
+            <p className="text-slate-500 text-sm">
+              {completedSubs}/{totalSubs} habits completed
+            </p>
+          </div>
+          <div>
+            <CircularProgressbar
+              percentage={percentajeCompleted}
+              strokeWidth={5}
+            />
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 };
