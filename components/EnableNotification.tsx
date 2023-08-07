f007e5f59476c1426549c1df3fa3bf01a1953ba9
MidoloS <midolo.1912@gmail.com>
2023-08-07 02:57:37 -0300

    I CAN SEE THE TOKEN

--- components/EnableNotification.tsx
+++ components/EnableNotification.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { Switch } from "./Switch";
 import { getMessaging, getToken } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
 
 // import { requestPermission } from "@/firebase/helpers";
 
@@ -23,32 +24,44 @@ export const EnableNotification = () => {
       console.log(res);
     })();
 
-    // if ("serviceWorker" in navigator && "PushManager" in window) {
-    //   const messaging = getMessaging();
-    //   console.log({ messaging });
-    //   // Add the public key generated from the console here.
-    //   getToken(messaging, {
-    //     vapidKey:
-    //       "BPq2545hDXGs4Gx2RqWw_dtokiqEQDjoG81YoUjV30j3wk5nZ9jwxK7_kj01Cwrm1h4tenvje8saelksUkVoSWs",
-    //   })
-    //     .then((currentToken) => {
-    //       console.log("currentToken: ", currentToken);
-    //       if (currentToken) {
-    //         // Send the token to your server and update the UI if necessary
-    //         // ...
-    //       } else {
-    //         // Show permission request UI
-    //         console.log(
-    //           "No registration token available. Request permission to generate one."
-    //         );
-    //         // ...
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       console.log("An error occurred while retrieving token. ", err);
-    //       // ...
-    //     });
-    // }
+    if ("serviceWorker" in navigator && "PushManager" in window) {
+      const firebaseConfig = {
+        apiKey: "AIzaSyATF4b77jYWBTyWUa70ONitSxUwZ7QtQCU",
+        authDomain: "habitai-391719.firebaseapp.com",
+        projectId: "habitai-391719",
+        storageBucket: "habitai-391719.appspot.com",
+        messagingSenderId: "104807834649",
+        appId: "1:104807834649:web:190cc2562190fc8894f688",
+        measurementId: "G-GPFDSMCCZW",
+      };
+
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const messaging = getMessaging(app);
+      console.log({ messaging });
+      // Add the public key generated from the console here.
+      getToken(messaging, {
+        vapidKey:
+          "BPq2545hDXGs4Gx2RqWw_dtokiqEQDjoG81YoUjV30j3wk5nZ9jwxK7_kj01Cwrm1h4tenvje8saelksUkVoSWs",
+      })
+        .then((currentToken) => {
+          console.log("currentToken: ", currentToken);
+          if (currentToken) {
+            // Send the token to your server and update the UI if necessary
+            // ...
+          } else {
+            // Show permission request UI
+            console.log(
+              "No registration token available. Request permission to generate one."
+            );
+            // ...
+          }
+        })
+        .catch((err) => {
+          console.log("An error occurred while retrieving token. ", err);
+          // ...
+        });
+    }
 
     window.addEventListener("load", () => {
       if ("serviceWorker" in navigator) {
@@ -56,7 +69,9 @@ export const EnableNotification = () => {
     });
     navigator.serviceWorker.ready.then((reg) => {
       reg.pushManager.getSubscription().then((sub) => {
-        if (!sub) {
+        console.log("sub", sub);
+
+        if (sub === null) {
           // Update UI to ask user to register for Push
           console.log("Not subscribed to push service!");
         } else {
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,41 +1,70 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.1.0/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging-compat.js"
-);
-
-console.log("root");
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyATF4b77jYWBTyWUa70ONitSxUwZ7QtQCU",
-  authDomain: "habitai-391719.firebaseapp.com",
-  projectId: "habitai-391719",
-  storageBucket: "habitai-391719.appspot.com",
-  messagingSenderId: "104807834649",
-  appId: "1:104807834649:web:190cc2562190fc8894f688",
-  measurementId: "G-GPFDSMCCZW",
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-console.log("messaging", messaging);
-
-messaging.onBackgroundMessage(messaging, (payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
+if ("serviceWorker" in navigator) {
+  importScripts(
+    "https://www.gstatic.com/firebasejs/10.1.0/firebase-app-compat.js"
   );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
-  const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
+  importScripts(
+    "https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging-compat.js"
+  );
+
+  console.log("root");
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyATF4b77jYWBTyWUa70ONitSxUwZ7QtQCU",
+    authDomain: "habitai-391719.firebaseapp.com",
+    projectId: "habitai-391719",
+    storageBucket: "habitai-391719.appspot.com",
+    messagingSenderId: "104807834649",
+    appId: "1:104807834649:web:190cc2562190fc8894f688",
+    measurementId: "G-GPFDSMCCZW",
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const messaging = firebase.messaging();
+
+  console.log("messaging", messaging);
+
+  messaging
+    .getToken(messaging, {
+      vapidKey:
+        "BPq2545hDXGs4Gx2RqWw_dtokiqEQDjoG81YoUjV30j3wk5nZ9jwxK7_kj01Cwrm1h4tenvje8saelksUkVoSWs",
+    })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log("TOKEN: " + currentToken);
+      } else {
+        // Show permission request UI
+        console.log(
+          "No registration token available. Request permission to generate one."
+        );
+        // ...
+      }
+    })
+    .catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
+      // ...
+    });
+
+  messaging.onBackgroundMessage(messaging, (payload) => {
+    console.log(
+      "[firebase-messaging-sw.js] Received background message ",
+      payload
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+      body: "Background Message body.",
+      icon: "/firebase-logo.png",
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
+
+  messaging.setBackgroundMessageHandler(function (payload) {
+    const title = "Hello World";
+    const option = { body: payload.data.status };
+    return self.registration.showNotification(title, option);
+  });
+}
