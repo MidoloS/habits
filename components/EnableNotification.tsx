4abf4a9ab59010d198a26100b92c17ec8ce04c8e
MidoloS <midolo.1912@gmail.com>
2023-08-24 21:45:51 -0300

    modal accept 2

--- components/EnableNotification.tsx
+++ components/EnableNotification.tsx
@@ -4,27 +4,39 @@ import { useEffect, useState } from "react";
 import { Switch } from "./Switch";
 import { getMessaging, getToken } from "firebase/messaging";
 import { initializeApp } from "firebase/app";
+import { createPortal } from "react-dom";
 
 // import { requestPermission } from "@/firebase/helpers";
 
 export const EnableNotification = () => {
-  const [permission, setPermission] = useState(false);
+  const [permission, setPermission] = useState(true);
+
+  function requestPermission() {
+    console.log("Requesting permission...");
+    Notification.requestPermission().then((a) => {
+      console.log({ a });
+      setPermission(a === "granted");
+
+      if (a === "granted") {
+        console.log("Notification permission granted.");
+      }
+    });
+  }
 
   useEffect(() => {
     console.log("useEffect");
 
-    (async () => {
-      console.log("navigator", navigator);
-      console.log("support", "serviceWorker" in navigator);
-
-      const res = await navigator.serviceWorker.register(
-        "/firebase-messaging-sw.js"
-      );
-      console.log("response");
-      console.log(res);
-    })();
-
     if ("serviceWorker" in navigator && "PushManager" in window) {
+      (async () => {
+        console.log("navigator", navigator);
+        console.log("support", "serviceWorker" in navigator);
+
+        const res = await navigator.serviceWorker.register(
+          "/firebase-messaging-sw.js"
+        );
+        console.log("response");
+        console.log(res);
+      })();
       const firebaseConfig = {
         apiKey: "AIzaSyATF4b77jYWBTyWUa70ONitSxUwZ7QtQCU",
         authDomain: "habitai-391719.firebaseapp.com",
@@ -76,36 +88,31 @@ export const EnableNotification = () => {
         });
       });
     });
+
+    requestPermission();
   }, []);
 
   if (permission) {
     return null;
   }
 
-  function requestPermission() {
-    console.log("Requesting permission...");
-    Notification.requestPermission().then((permission) => {
-      console.log({ permission });
-
-      if (permission === "granted") {
-        console.log("Notification permission granted.");
-      }
-    });
-  }
-
-  return (
-    <div className="absolute">
-      <div className="bg-slate-950 flex justify-between items-center p-4 rounded-lg w-screen">
-        <div className="flex flex-col gap-1">
-          <h3 className="text-slate-50 font-medium">
-            Wanna improve your habits?
-          </h3>
-          <p className="text-slate-300 text-sm">Enable Daily Reminder</p>
-        </div>
+  return createPortal(
+    <div className="absolute top-0 left-0 bg-slate-950 bg-opacity-80 w-screen h-screen z-50 p-8 flex justify-center items-center">
+      <div className="bg-slate-50 p-6 rounded-xl relative flex flex-col gap-4 max-w-md">
         <div>
-          <Switch onClick={requestPermission} />
+          <p className="text-center text-slate-500 text-sm mb-2">
+            Would you like to improve?
+          </p>
+          <h1 className="font-heading text-lg font-semibold text-center">
+            Daily Reminder's
+          </h1>
         </div>
+        <p>
+          People who turn on notifications are 60% more likely to compel their
+          habits.
+        </p>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
