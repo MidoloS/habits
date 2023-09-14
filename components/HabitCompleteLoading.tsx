e79c1cd3ae62f7f22a388fd4feb40b9e2a7e27bc
MidoloS <midolo.1912@gmail.com>
2023-09-14 13:05:33 -0300

    better proccesing page

--- components/HabitCompleteLoading.tsx
+++ components/HabitCompleteLoading.tsx
@@ -1,15 +1,25 @@
+import Image from "next/image";
 import { Loading } from "./Info/Loading";
 
 export const HabitCompleteLoading = () => (
-  <article className="h-screen w-screen z-50 bg-slate-950 flex flex-col gap-8 absolute top-0 left-0 justify-center items-center">
+  <article className="h-screen w-screen z-50 bg-slate-50 flex flex-col gap-8 absolute top-0 left-0 justify-center items-center">
     <div>
-      <Loading />
+      <Image
+        src="/forest-loading.png"
+        width={300}
+        height={300}
+        alt="vectorial forest"
+        style={{ borderRadius: 12 }}
+      />
     </div>
     <div className="text-center flex flex-col gap-1">
-      <h1 className="font font-heading text-slate-50 text-2xl font-semibold">
-        Processing...
+      <h1 className="font font-heading text-slate-950 text-2xl font-semibold">
+        Processing Image...
       </h1>
-      <p className="text-slate-500">This might take a few seconds</p>
+      <p className="text-slate-500 text-sm">This might take a few seconds</p>
+    </div>
+    <div>
+      <Loading size="sm" />
     </div>
   </article>
 );
