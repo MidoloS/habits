7bab313fad1866e9937c49af6dfc34947fdda0ce
MidoloS <midolo.1912@gmail.com>
2023-09-16 23:55:11 -0300

    fix label for new verb

--- libs/constants.ts
+++ libs/constants.ts
@@ -2,7 +2,7 @@ export const API_TO_HABIT_NAME = {
   "washing machine": "laundry",
   "brushing teeth": "brush",
   "person drinking water": "drink",
-  "made bed": "tidy",
+  "neat bed": "tidy",
   fruit: "eat",
   park: "walk",
 };
