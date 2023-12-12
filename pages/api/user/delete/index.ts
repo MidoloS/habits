3e4b7d61353b80eb03472e458c8021613e945eeb
MidoloS <midolo.1912@gmail.com>
2023-12-12 19:56:02 -0300

    delete api

---
+++ pages/api/user/delete/index.ts
@@ -0,0 +1,25 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { generatePrismaClient } from "@/prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = generatePrismaClient();
+
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return res.status(404).json({ message: "Session not started" });
+  }
+
+  const user = await prisma.user.delete({
+    where: {
+      email: session?.user?.email,
+    },
+  });
+
+  return res.status(200).json({ message: "User deleted", data: user });
+}
