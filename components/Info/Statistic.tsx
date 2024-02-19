e070026572d33d5a12bd6e681a8784d39e3567fd
MidoloS <midolo.1912@gmail.com>
2024-02-18 23:11:54 -0300

    small statistic

--- components/Info/Statistic.tsx
+++ components/Info/Statistic.tsx
@@ -8,6 +8,6 @@ type Props = {
 export const Statistic: FC<Props> = ({ description, value }) => (
   <section className="flex flex-col items-center">
     <p className="font-bold text-xl text-zinc-50">{value}</p>
-    <p className="text-zinc-400 text-sm">{description}</p>
+    <p className="text-zinc-400 text-xs">{description}</p>
   </section>
 );
